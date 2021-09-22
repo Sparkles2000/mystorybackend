518429beb2e54cf26736ab0e93958f94816c9724
Sparkles2000 <rucker.anna2000@gmail.com>
2021-09-22 15:47:25 -0500

    Updated another error in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
     def index
         users = User.all
-        render json: users
         render json: users, only: [:alias, :img_url, :age, :story]
     end
     def show
