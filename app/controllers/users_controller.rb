7411fa0e85b1064544e7bb569b76fe4d8a3ecc3b
Sparkles2000 <rucker.anna2000@gmail.com>
2021-09-22 13:01:34 -0500

    Added new data into params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:alias)
+        params.permit(:alias :img_url, :age, :story)
     end  
 end
