079d1bc5ad099164bf3dab519c14150592982ee5
Sparkles2000 <rucker.anna2000@gmail.com>
2021-09-24 17:50:30 -0500

    Reverted the nested routes back in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :posts
-  end
-  resources :posts do
-    resources :tags
-    resources :comments
-  end
-  resources :tags do
-    resources :posts
-  end
+  resources :users
+  resources :posts
+  resources :tags
   resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
